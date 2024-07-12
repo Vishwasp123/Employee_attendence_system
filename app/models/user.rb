94b2cfc2147fba460f20d108a767b6906dc94844
shivam123 <shivamchouhan175@gmail.com>
2024-07-12 20:07:31 +0530

    Add signup and login user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+  has_secure_password
+  # validates :name, presence: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :password_digest, presence: true
+  # validates :gender, inclusion: { in: ['male', 'female', 'other'] }
+  # # validates :password, length: { minimum: 6 }, allow_nil: true
+  # validates :active, inclusion: { in: [true, false] }
 end
--- db/migrate/20240712074959_create_users.rb
+++ db/migrate/20240712074959_create_users.rb
@@ -1,7 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :user_name
+      t.string :email
+      t.string :phone_number
+      t.string :gender
+      t.boolean :active, default: false 
+      t.string :password_digest
       t.timestamps
     end
   end
